7cd25fe368ee43e76d3dee3c60ec0941e5586404
Marcos Vinicius <marcos.ferreira@dcx.ufpb.br>
2023-05-30 12:52:11 -0300

    alterações

--- src/gestures.js
+++ src/gestures.js
@@ -37,12 +37,11 @@ for(let finger of [Finger.Thumb, Finger.Index, Finger.Ring, Finger.Pinky]) {
     middleFingerGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
     middleFingerGesture.addCurl(finger, FingerCurl.HalfCurl, 0.9);
 }
-// thumb: half curled
-// accept no curl with a bit lower confidence
+
 rockGesture.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 1.0);
 rockGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl, 0.5);
 
-// all other fingers: curled
+
 for(let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
     rockGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
     rockGesture.addCurl(finger, FingerCurl.HalfCurl, 0.9);
@@ -53,7 +52,7 @@ for(let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
 // Paper
 // -----------------------------------------------------------------------------
 
-// no finger should be curled
+
 for(let finger of Finger.all) {
     paperGesture.addCurl(finger, FingerCurl.NoCurl, 1.0);
 }
@@ -62,15 +61,15 @@ for(let finger of Finger.all) {
 // Scissors
 //------------------------------------------------------------------------------
 
-// index and middle finger: stretched out
+
 scissorsGesture.addCurl(Finger.Index, FingerCurl.NoCurl, 1.0);
 scissorsGesture.addCurl(Finger.Middle, FingerCurl.NoCurl, 1.0);
 
-// ring: curled
+
 scissorsGesture.addCurl(Finger.Ring, FingerCurl.FullCurl, 1.0);
 scissorsGesture.addCurl(Finger.Ring, FingerCurl.HalfCurl, 0.9);
 
-// pinky: curled
+
 scissorsGesture.addCurl(Finger.Pinky, FingerCurl.FullCurl, 1.0);
 scissorsGesture.addCurl(Finger.Pinky, FingerCurl.HalfCurl, 0.9);
 
